98a4867b6e0b2d3a0e7ec27f475fde747aa88dae
Ian <muneneian5@gmail.com>
2022-12-02 10:15:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name){
+    let newArray = [];
+    for(let i = 0; i < name.length; i++){
+        let message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+let count;
+function countDown(count){
+    while(count >=0){
+        console.log(count);
+        count--;
+    }
+}
+countDown(count)
